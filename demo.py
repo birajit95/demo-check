3233a66ae7898a1d4d73e040aaac85be74002f12
Ranjith P <Ranjithroy704@gmail.com>
2021-02-08 19:39:51 +0530

    Development push

--- demo.py
+++ demo.py
@@ -9,3 +9,16 @@ class Hello:
 if __name__ == '__main__':
 	obj = Hello('Birajit')
 	print(obj.demo_fun())
+
+
+class Hello:
+        def __init__(self, name):
+                self.name = name
+
+        def demo_fun(self):
+                return f" Hello {self.name}"
+
+
+if __name__ == '__main__':
+        obj = Hello('Ranjith')
+        print(obj.demo_fun())
